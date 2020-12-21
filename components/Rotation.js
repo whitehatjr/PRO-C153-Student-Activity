71bd3397cae1b41649a3c22d6c2e50e41f6c21b8
whitehatjr <52084703+whitehatjr@users.noreply.github.com>
2020-12-21 18:48:41 +0530

    Add files via upload

---
+++ components/Rotation.js
@@ -0,0 +1,35 @@
+//Terrain Rotation
+AFRAME.registerComponent("terrain-rotation-reader", {
+  schema: {
+    speedOfRoation: { type: "number", default: 0 }    
+  },
+  init: function () {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "ArrowRight") {
+        if (this.data.speedOfRoation < 0.1) {
+          this.data.speedOfRoation += 0.01;
+        }
+      }
+      if (e.key === "ArrowLeft") {
+        if (this.data.speedOfRoation > -0.1) {
+          this.data.speedOfRoation -= 0.01;
+        }
+      }
+    });
+  },
+  tick: function () {
+    var mapRotation = this.el.getAttribute("rotation");
+
+    mapRotation.y += this.data.speedOfRoation;
+
+    this.el.setAttribute("rotation", {
+      x: mapRotation.x,
+      y: mapRotation.y,
+      z: mapRotation.z
+    });
+  }
+});
+
+
+
+
